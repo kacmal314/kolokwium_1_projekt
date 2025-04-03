8c2e306b61e5b706e789aef26e22812f726799ea
Aleksander Chomicz <xchomiczo@gmail.com>
2025-04-03 23:38:55 +0200

    Pierwsze zadanie - do weryfikacji wyników

---
+++ zadanie_dla_1_Olek.py
@@ -0,0 +1,19 @@
+﻿import math
+
+# zmienne - dane
+srednicaRurociagu = 0.1  # średnica w metrach
+gestoscCieczy = 1200      # gęstość w kg/m³
+roznicaCisnien = 350      # różnica ciśnień w Pa
+
+def WyznaczenieStrumienObjetosci(srednicaRurociagu, gestoscCieczy, roznicaCisnien):
+    pole_przekroju = math.pi * (srednicaRurociagu ** 2) / 4
+    predkosc = math.sqrt((2 * roznicaCisnien) / gestoscCieczy)
+    wyn = predkosc * pole_przekroju
+    return wyn
+
+def WyznaczenieStrumienMasy(gestoscCieczy, strumienObjetosci):
+    wyn = gestoscCieczy * strumienObjetosci
+    return wyn
+
+print("V objętości: ",WyznaczenieStrumienObjetosci(srednicaRurociagu, gestoscCieczy, roznicaCisnien))
+print("V masy: ",WyznaczenieStrumienMasy(gestoscCieczy, WyznaczenieStrumienObjetosci(srednicaRurociagu, gestoscCieczy, roznicaCisnien)))
